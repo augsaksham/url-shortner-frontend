cbb38c9d06a8255b5d47a6163fd1665e8ed65209
augsaksham <sakshamdwivedi10@gmail.com>
2025-03-03 10:17:50 +0530

    Added auth component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent {
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+  isLoginMode = true;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    if (this.isLoginMode) {
+      this.authService.login(this.loginForm.value.email, this.loginForm.value.password)
+        .subscribe(response => {
+          localStorage.setItem('token', response['token']);
+          this.router.navigate(['/dashboard']);
+        }, error => {
+          alert('Login failed: ' + error.message);
+        });
+    } else {
+      this.authService.register(this.registerForm.value.username, this.registerForm.value.email, this.registerForm.value.password)
+        .subscribe(response => {
+          alert('Registration successful');
+          this.onSwitchMode();
+        }, error => {
+          alert('Registration failed: ' + error.message);
+        });
+    }
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private baseUrl = 'http://localhost:8000/api/v1/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    const body = new URLSearchParams();
+    body.set('grant_type', '');
+    body.set('username', username);
+    body.set('password', password);
+    body.set('scope', '');
+    body.set('client_id', '');
+    body.set('client_secret', '');
+
+    return this.http.post(`${this.baseUrl}/token`, body.toString(), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'accept': 'application/json'
+      }
+    });
+  }
+
+  register(username: string, email: string, password: string): Observable<any> {
+    const body = { username, email, password };
+    return this.http.post(`${this.baseUrl}/register`, body, {
+      headers: {
+        'Content-Type': 'application/json',
+        'accept': 'application/json'
+      }
+    });
+  }
+}
